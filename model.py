fc5ddfe3ab2038c5601414161bf78e7d8aa271c2
williamliusea <williamliu_sea@hotmail.com>
2019-03-12 05:55:07 +0000

    initial check in

---
+++ model.py
@@ -0,0 +1,152 @@
+import csv
+import cv2
+import numpy as np
+import sklearn
+
+# def generator(samples, batch_size=32):
+#     num_samples = len(samples)
+#     while 1: # Loop forever so the generator never terminates
+#         shuffle(samples)
+#         for offset in range(0, num_samples, batch_size):
+#             batch_samples = samples[offset:offset+batch_size]
+
+#             images = []
+#             angles = []
+#             for batch_sample in batch_samples:
+#                 name = basedir + '/IMG/'+batch_sample[0].split('/')[-1]
+#                 center_image = cv2.imread(name)
+#                 center_angle = float(batch_sample[3])
+#                 images.append(center_image)
+#                 angles.append(center_angle)
+
+#             # trim image to only see section with road
+#             X_train = np.array(images)
+#             y_train = np.array(angles)
+#             yield sklearn.utils.shuffle(X_train, y_train)
+
+# Set our batch size
+batch_size=32
+
+def preprocess_item(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    g_max=np.amax(gray)
+    g_min=np.amin(gray)
+    g = np.divide(np.subtract(gray, g_min), g_max-g_min)
+    return np.dstack((g))
+
+def getImage(line, correction, which):
+    if (which=='right'):
+        src_path=line[2]
+        steering = float(line[3]) - correction
+    elif (which=='left'):
+        src_path=line[1]
+        steering = float(line[3]) + correction
+    else:
+        src_path=line[0]
+        steering = float(line[3])
+    filename=src_path.split('/')[-1]
+    cur_path='data/IMG/'+filename
+    image=cv2.imread(cur_path)    
+#     image=preprocess_item(cv2.imread(cur_path))
+#     print(image.shape)
+    return image,steering
+    
+lines =[]
+with open('data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    skip_first=False
+    for line in reader:
+        if (skip_first):
+            lines.append(line)
+        else:
+            skip_first=True
+
+basedir = 'data'
+samples = []
+# with open(basedir+'/driving_log.csv') as csvfile:
+#     reader = csv.reader(csvfile)
+#     for line in reader:
+#         samples.append(line)
+
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+images =[]
+measurements=[]
+count=0
+for line in lines:
+    image,steering=getImage(line, 0.05, 'left')
+    images.append(image)
+    image_flipped=np.fliplr(image)
+    images.append(image_flipped)
+    measurements.append(steering)
+    measurements.append(-steering)
+    image,steering=getImage(line, 0.05, 'center')
+    images.append(image)
+    image_flipped=np.fliplr(image)
+    images.append(image_flipped)
+    measurements.append(steering)
+    measurements.append(-steering)
+    image,steering=getImage(line, 0.05, 'right')
+    images.append(image)
+    image_flipped=np.fliplr(image)
+    images.append(image_flipped)
+    measurements.append(steering)
+    measurements.append(-steering)
+
+X_train = np.array(images)
+y_train = np.array(measurements)
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import MaxPooling2D
+
+
+# simple regression
+# model = Sequential()
+# model.add(Flatten(input_shape=(160,320,3)))
+# model.add(Dense(1))
+# model.compile(loss='mse', optimizer='adam')
+# model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+
+# LeNet
+model = Sequential()
+model.add(Cropping2D(cropping=((50,20),(0,0)), input_shape=(160, 320, 3)))
+model.add(Lambda(lambda x: x/255.0-0.5))
+model.add(Convolution2D(6,5,5,activation='relu'))
+model.add(MaxPooling2D())
+model.add(Dropout(0.5))
+model.add(Convolution2D(24,3,3,activation='relu'))
+model.add(MaxPooling2D())
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(120))
+model.add(Dense(84))
+model.add(Dense(1))
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
+
+# NVIDIA
+# model = Sequential()
+# model.add(Cropping2D(cropping=((50,20),(0,0)), input_shape=(160, 320, 3)))
+# model.add(Lambda(lambda x: x/255.0-0.5))
+# model.add(Convolution2D(3,5,5,activation='relu'))
+# model.add(MaxPooling2D())
+# model.add(Convolution2D(24,5,5,activation='relu'))
+# model.add(Convolution2D(36,5,5,activation='relu'))
+# model.add(Convolution2D(48,3,3,activation='relu'))
+# model.add(Convolution2D(64,3,3,activation='relu'))
+# model.add(MaxPooling2D())
+# model.add(Flatten())
+# model.add(Dense(100))
+# model.add(Dense(50))
+# model.add(Dense(10))
+# model.add(Dense(1))
+# model.compile(loss='mse', optimizer='adam')
+# model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=1)
+# 164s - loss: 0.0184 - val_loss: 0.0104
+
+model.save('model.h5')
+exit()
+
+
