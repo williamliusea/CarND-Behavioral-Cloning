9651b097e181bb4c386f25290fc4fadbd61c5e0f
root <you@example.com>
2019-03-20 05:41:40 +0000

    add generator

--- model.py
+++ model.py
@@ -3,12 +3,18 @@ import cv2
 import numpy as np
 import numpy.random
 import sklearn
+import math
 
-basedir = '/opt/data'
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from sklearn.model_selection import train_test_split
+
 def generator(samples, batch_size=32):
     num_samples = len(samples)
     while 1: # Loop forever so the generator never terminates
-        shuffle(samples)
+        sklearn.utils.shuffle(samples)
         for offset in range(0, num_samples, batch_size):
             batch_samples = samples[offset:offset+batch_size]
 
@@ -25,9 +31,6 @@ def generator(samples, batch_size=32):
             X_train = np.array(images)
             y_train = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
-
-# Set our batch size
-# batch_size=32
     
 def load_data():
     for line in lines:
@@ -36,37 +39,16 @@ def load_data():
         images.append(image)
         measurements.append(steering)
 
-# from sklearn.model_selection import train_test_split
-# train_samples, validation_samples = train_test_split(samples, test_size=0.2)
-
-lines =[]
-with open(basedir+'/driving_log.csv') as csvfile:
-    reader = csv.reader(csvfile)
-    skip_first=False
-    for line in reader:
-        if (skip_first):
-            lines.append(line)
-        else:
-            skip_first=True
-
-images =[]
-measurements=[]
-load_data()
-
-X_train = np.array(images)
-y_train = np.array(measurements)
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
-from keras.layers.convolutional import Convolution2D
-from keras.layers.pooling import MaxPooling2D
-
 def nvidia(input_shape):
+    # compile and train the model using the generator function
+    train_generator = generator(train_samples, batch_size=batch_size)
+    validation_generator = generator(validation_samples, batch_size=batch_size)
     model = Sequential()
     model.add(Lambda(lambda x: x/127.5 - 1.0, input_shape=input_shape))
     model.add(Conv2D(3,(5,5),activation='relu'))
     model.add(MaxPooling2D())
     model.add(Conv2D(24,(5,5),activation='relu'))
-    model.add(Conv2D(36,(5,5,activation='relu'))
+    model.add(Conv2D(36,(5,5),activation='relu'))
     model.add(Conv2D(48,(3,3),activation='relu'))
     model.add(Conv2D(64,(3,3),activation='relu'))
     model.add(Dropout(0.5))
@@ -76,10 +58,13 @@ def nvidia(input_shape):
     model.add(Dense(50))
     model.add(Dense(1))
     model.compile(loss='mse', optimizer='adam')
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=3)
+    model.fit_generator(train_generator, steps_per_epoch=math.ceil(len(train_samples)/batch_size), validation_data=validation_generator, validation_steps=math.ceil(len(validation_samples)/batch_size), epochs=2)
     model.save('model.nvidia.h5')
 
 def kasper(input_shape):
+    # compile and train the model using the generator function
+    train_generator = generator(train_samples, batch_size=batch_size)
+    validation_generator = generator(validation_samples, batch_size=batch_size)
     model = Sequential()
     model.add(Lambda(lambda x: x/127.5 - 1.0, input_shape=input_shape))
     model.add(Conv2D(32, (8,8) ,border_mode='same', subsample=(4,4),activation='relu'))
@@ -93,10 +78,14 @@ def kasper(input_shape):
     model.add(Dense(128))
     model.add(Dense(1))
     model.compile(loss='mse', optimizer='adam')
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=5)
+    model.fit_generator(train_generator, steps_per_epoch=math.ceil(len(train_samples)/batch_size), validation_data=validation_generator, validation_steps=math.ceil(len(validation_samples)/batch_size), epochs=3)
     model.save('model.kasper.h5')
  
 def lenet(input_shape):
+    # compile and train the model using the generator function
+    train_generator = generator(train_samples, batch_size=batch_size)
+    validation_generator = generator(validation_samples, batch_size=batch_size)
+
     model = Sequential()
     model.add(Lambda(lambda x: x/127.5 - 1.0, input_shape=input_shape))
     model.add(Conv2D(6,(5,5),activation='relu'))
@@ -109,10 +98,30 @@ def lenet(input_shape):
     model.add(Dense(84))
     model.add(Dense(1))
     model.compile(loss='mse', optimizer='adam')
-    model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs=10)
+    model.fit_generator(train_generator, steps_per_epoch=math.ceil(len(train_samples)/batch_size), validation_data=validation_generator, validation_steps=math.ceil(len(validation_samples)/batch_size), epochs=5)
     model.save('model.lenet.h5')
 
+batch_size=320 
 shape = (90, 320,3)
+lines =[]
+basedir = '/opt/data'
+with open(basedir+'/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    skip_first=False
+    for line in reader:
+        if (skip_first):
+            lines.append(line)
+        else:
+            skip_first=True
+
+# images =[]
+# measurements=[]
+train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+print(len(train_samples),len(validation_samples), len(lines))
+# load_data()
+
+# X_train = np.array(images)
+# y_train = np.array(measurements)
 lenet(shape)
 nvidia(shape)
 kasper(shape)
