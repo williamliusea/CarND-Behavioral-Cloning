1c37a69c17da5fa860ef7334d8de6ee1968f757d
root <you@example.com>
2019-03-21 05:50:40 +0000

    full size video

--- drive.py
+++ drive.py
@@ -59,8 +59,8 @@ def telemetry(sid, data):
         speed = data["speed"]
         # The current image from the center camera of the car
         imgString = data["image"]
-        image = Image.open(BytesIO(base64.b64decode(imgString)))
-        image = image.crop((0,50,320,140))
+        image_raw = Image.open(BytesIO(base64.b64decode(imgString)))
+        image = image_raw.crop((0,50,320,140))
 #         image.save("1.png")
         image = image.resize(shape, Image.ANTIALIAS)
 #         nparr = np.fromstring(base64.b64decode(imgString), np.uint8)
@@ -78,7 +78,7 @@ def telemetry(sid, data):
         if args.image_folder != '':
             timestamp = datetime.utcnow().strftime('%Y_%m_%d_%H_%M_%S_%f')[:-3]
             image_filename = os.path.join(args.image_folder, timestamp)
-            image.save('{}.jpg'.format(image_filename))
+            image_raw.save('{}.jpg'.format(image_filename))
     else:
         # NOTE: DON'T EDIT THIS.
         sio.emit('manual', data={}, skip_sid=True)
