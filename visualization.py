c5a4b48499707ec1288f0c37918f3d41642630cb
root <you@example.com>
2019-03-25 05:24:24 +0000

    update

---
+++ visualization.py
@@ -0,0 +1,30 @@
+import argparse
+
+from flask import Flask
+from io import BytesIO
+
+from keras.models import load_model
+import h5py
+from keras import __version__ as keras_version
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Visulization')
+    parser.add_argument(
+        'model',
+        type=str,
+        help='Path to model h5 file. Model should be on the same path.'
+    )
+    args = parser.parse_args()
+
+    # check that model Keras version is same as local Keras version
+    f = h5py.File(args.model, mode='r')
+    model_version = f.attrs.get('keras_version')
+    keras_version = str(keras_version).encode('utf8')
+
+    if model_version != keras_version:
+        print('You are using Keras version ', keras_version,
+              ', but the model was built using ', model_version)
+
+    model = load_model(args.model)
+    
+    model.summary()
